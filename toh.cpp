d9d7af8e8523642886df021de0b52504dc455838
Akash Mourya <mouryakash883@gmail.com>
2021-06-12 02:57:21 +0530

    Recursion

---
+++ toh.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void toh(int n,char src,char dest,char helper)
+{
+    if(n==0)
+    return;
+    toh(n-1,src,helper,dest);
+    cout<<"Move from "<<src<<" to "<<dest<<endl;
+    toh(n-1,helper,dest,src);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    toh(n,'A','C','B');
+}
