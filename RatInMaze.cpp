913225ae8b67fc5f2c078c3278ee080233112f04
Akash Mourya <mouryakash883@gmail.com>
2021-07-25 01:24:35 +0530

    Recursion

--- RatInMaze.cpp
+++ RatInMaze.cpp
@@ -1,66 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool print_maze(char maze[10][10],int sol[][10],int m,int n,int i,int j)
+int xMove[4] = { -1,1,0,0 };
+int yMove[4] = { 0,0,1,-1 };
+bool canPlace(char maze[8][8],int i, int j, int n)
 {
-    if(i==m && j==n)
+    if(i<0 || j<0 || i>=n || j>=n || maze[i][j]=='1' || maze[i][j]=='X')
+        return false;
+    return true;
+}
+bool solve(char maze[8][8], int i, int j, int n, int m)
+{
+    if(i==n-1 && j==m-1)
     {
-        sol[m][n]=1;
-        //Print the path
-        for(int i=0;i<=m;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<=n;j++)
-            cout<<sol[i][j]<<" ";
+            for(int j=0;j<m;j++)
+                cout<<maze[i][j]<<" ";
             cout<<endl;
         }
         cout<<endl;
-        return true;
+       return true;
     }
 
-    //Rat is outside the grid
-    if(i>m || j>n || i<0 || j<0)
+    for(int x=0;x<4;x++)
     {
-        return false;
+        if(canPlace(maze,i+xMove[x],j+yMove[x],n))
+        {
+            maze[i+xMove[x]][j+yMove[x]]='1';
+            solve(maze,i+xMove[x],j+yMove[x],n,m);
+                
+              maze[i+xMove[x]][j+yMove[x]]='0';  
+        }
     }
-    //Path is blocked
-    if(maze[i][j]=='X' || sol[i][j]==1)
-    {
-        return false;
-    }
-
-    //Assuming solution exists through current cell
-    sol[i][j]=1;
-
-    //Check if rat can move in right block
-    bool right_success=print_maze(maze,sol,m,n,i,j+1);
-    //Check if rat can move in down block
-    bool down_success=print_maze(maze,sol,m,n,i+1,j);
-    //Check if rat can move in up block
-    bool up_success=print_maze(maze,sol,m,n,i-1,j);
-    //Check if rat can move in left block
-    bool left_success=print_maze(maze,sol,m,n,i,j-1);
-
-    
-    //Backtracking
-    sol[i][j]=0;
-
-    if(right_success || down_success || up_success || left_success)
-    return true;
-
     return false;
 }
 int main()
 {
-    char maze[10][10]={
-        "00X",
+    char maze[8][8]={
+        "10X",
         "000",
         "000"
     };
-    int sol[10][10]={0};
     int m=3,n=3;
-    bool ans=print_maze(maze,sol,m-1,n-1,0,0);
+    bool ans=solve(maze,0,0,m,n);
     if(!ans)
     {
         cout<<"Path does not exist";
     }
-    
 }
